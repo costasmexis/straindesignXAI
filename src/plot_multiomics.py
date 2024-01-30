4ceebadf116c2d7ebfb6181e973e5ca4f33d7af0
costasmexis <costasmexis@gmail.com>
2024-01-30 14:44:10 +0200

    Update

--- src/plot_multiomics.py
+++ src/plot_multiomics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Times New Roman'
 
 n_modifications = 3
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Times New Roman'
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.model_selection import cross_val_score, cross_validate, cross_val_predict, GridSearchCV
